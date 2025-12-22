6a81aec25ebbaf06706fc512d6c9edf1b0aba04d
JustinYSJSU <justinkaiy02@gmail.com>
2025-12-21 18:15:41 -0800

    basic triage for mission length and status counting

--- src/triage.py
+++ src/triage.py
@@ -4,4 +4,50 @@ from pathlib import Path
 current_dir = Path(__file__).parent.parent # 'root directory'
 robot_data = current_dir / "data" / "robot_data.log"
 
-pd.read_csv(robot_data)
+df = pd.read_csv(robot_data, sep='|', names=['timestamp', 'component_status', 'component', 'component_status_message'])
+
+def calculate_mission_time(df):
+    '''
+    Calculate total time of the mission
+    
+    :param fd: data frame from pandas read_csv
+    '''
+
+    # in dataframe, timestamp is type str
+
+    mission_start_time = pd.to_datetime(df['timestamp'].iloc[0]) # from the timestamp column, get the 0th (first) index
+    mission_end_time = pd.to_datetime(df['timestamp'].iloc[-1]) # from the timestamp column, get the last index
+
+    duration = mission_end_time - mission_start_time
+    seconds = int(duration.total_seconds())
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    mission_time_string = f"{hours} hours {minutes} minutes {seconds} seconds"
+    return mission_time_string
+
+def calculate_status_count(df):
+    '''
+    Calculate total count of each status (INFO, WARN, ERROR)
+    
+    :param fd: data frame from pandas read_csv
+    '''
+    status_dict = {}
+
+    for row in df.itertuples():
+        if row.component_status in status_dict:
+            status_dict[row.component_status] += 1
+        else:
+            status_dict[row.component_status] = 1
+    return status_dict
+
+def calculate_culprints(df):
+    '''
+    Calculate the "culprit" of each component status
+    "culprit" refers to the component with the highest count of a status
+    
+    :param fd: data frame from pandas read_csv
+    '''
+    culprit_dict = {}
+    
